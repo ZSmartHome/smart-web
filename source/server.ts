9b97ded6cfef3e129c9a2e8b155ca1c8a331f09f
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2019-05-09 21:28:06 +0300

    add pug setup server

--- source/server.ts
+++ source/server.ts
@@ -1,8 +1,15 @@
 import * as express from "express";
+import * as path from "path";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.set("views", path.join(__dirname, "../view"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "../static")));
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
