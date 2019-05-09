7cca4f913da51424fb7153cc81985c17165da292
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2019-05-09 22:29:43 +0300

    parse body

--- source/server.ts
+++ source/server.ts
@@ -1,8 +1,13 @@
 import * as express from 'express';
 import * as path from 'path';
 
+import * as bodyParser from 'body-parser';
+
 const app = express();
 const port = 3000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('views', path.join(__dirname, '../view'));
 app.set('view engine', 'pug');
@@ -10,6 +15,11 @@ app.use(express.static(path.join(__dirname, '../static')));
 
 app.get('/', (req, res) => {
   res.render('index', {title: 'SmartHome Web'});
+});
+
+app.post('/command', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
