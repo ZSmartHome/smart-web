3264aef4d6d2fd9b6b6061f6f75834d9dcaa9310
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2019-05-09 22:21:31 +0300

    fix lint

--- source/server.ts
+++ source/server.ts
@@ -1,15 +1,15 @@
-import * as express from "express";
-import * as path from "path";
+import * as express from 'express';
+import * as path from 'path';
 
 const app = express();
 const port = 3000;
 
-app.set("views", path.join(__dirname, "../view"));
-app.set("view engine", "pug");
-app.use(express.static(path.join(__dirname, "../static")));
+app.set('views', path.join(__dirname, '../view'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, '../static')));
 
-app.get('/', function (req, res) {
-  res.render('index', {title: 'SmartHome Web'})
+app.get('/', (req, res) => {
+  res.render('index', {title: 'SmartHome Web'});
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
