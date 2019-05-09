b07555b158bd99cd4ee6bb83abb13ff442ad3f8d
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2019-05-09 23:46:30 +0300

    added pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+
+    // First application
+    {
+      name: 'SmartHome Web',
+      script: 'build/server.js'
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    dev: {
+      user: 'pi',
+      host: 'raspberrypi',
+      ref: 'origin/master',
+      repo: 'https://github.com/zeckson/smart-web.git',
+      path: '/home/pi/smart-web',
+      'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env dev'
+    }
+  }
+};
