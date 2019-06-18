646782210dacdd5ee3f22caf7c472071a7fdbef2
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2019-06-18 23:55:49 +0300

    add gulp build add fontawesome

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+
+const paths = {
+  icons: {
+    src: [
+      'node_modules/@fortawesome/fontawesome-free/svgs/solid/power-off.svg',
+    ],
+    dest: 'static/icons/'
+  }
+};
+
+const svgo = require('gulp-svgo');
+const svgstore = require('gulp-svgstore');
+gulp.task('icons', () => {
+  return gulp.src(paths.icons.src)
+    .pipe(svgo())
+    .pipe(svgstore())
+    .pipe(rename('bundle.min.svg'))
+    .pipe(gulp.dest(paths.icons.dest));
+});
+
+
+gulp.task('copy', gulp.parallel('icons'));
+
+const del = require('del');
+gulp.task('clean', () => del(['build']));
+
+gulp.task('build', gulp.series('clean', 'copy'));
